65f6ee8702b5c335bc60cd2b89dd2a9315b26977
Fabrizzio <fabrizziorp7@gmail.com>
2022-07-17 21:31:38 -0400

    Environment db changes

--- database/config.js
+++ database/config.js
@@ -5,11 +5,11 @@ const connectPg = () => {
 
     try{
         const pool = new Pool({
-            host: process.env.HOST,
-            user: process.env.USER,
-            password: process.env.PASSWORD,
-            database: process.env.DATABASE,
-            port: process.env.PORTDB
+            host: process.env.HOSTPG,
+            user: process.env.USERPG,
+            password: process.env.PASSWORDPG,
+            database: process.env.DATABASEPG,
+            port: process.env.PORTPG
         });
         console.log("CONNECTED DATABASE :)");
         return pool;
