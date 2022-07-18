fe93582acdba0d3c3d6e1d2787508b1d2d37114a
Fabrizzio <fabrizziorp7@gmail.com>
2022-07-17 21:28:18 -0400

    dbport change

--- database/config.js
+++ database/config.js
@@ -9,7 +9,7 @@ const connectPg = () => {
             user: process.env.USER,
             password: process.env.PASSWORD,
             database: process.env.DATABASE,
-            port: process.env.PORT
+            port: process.env.PORTDB
         });
         console.log("CONNECTED DATABASE :)");
         return pool;
